4b885c8cb5635df011b078e118945bae0e21d1d2
Timothee-BRANDT <brandt.timothee.42nice@gmail.com>
2024-10-22 13:45:34 +0200

    .

---
+++ mobileModule00/ex00/app/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import { Image, StyleSheet, Platform, View, Button } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+    const clicked =  () => {
+        console.log("Button pressed");
+    }
+
+  return (
+    <View style={styles.container}>
+      <ThemedText style={styles.title}>Foo Bar</ThemedText>
+      <Button title="Try me" onPress={clicked}></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+  },
+});
